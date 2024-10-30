b1a6b1d2c1aacd0a0c87bf5fc9b70182c2d4b841
Dane Reid <danereid@macbookpro.mynetworksettings.com>
2024-10-30 09:59:12 -0400

    nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './pages/nav-bar/nav-bar.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
