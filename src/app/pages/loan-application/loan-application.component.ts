da7bf03c5b023049bca8e890bf36890728c0fd9d
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-11-01 14:30:46 -0400

    Add the loan to the application before submitting

--- src/app/pages/loan-application/loan-application.component.ts
+++ src/app/pages/loan-application/loan-application.component.ts
@@ -1,3 +1,45 @@
+// import { Component, inject } from '@angular/core';
+// import { FormsModule } from '@angular/forms';
+// import {
+//   ApiResponseModel,
+//   Application,
+//   Loan,
+// } from '../../model/application.model';
+// import { MainService } from '../../service/main.service';
+// @Component({
+//   selector: 'app-loan-application',
+//   standalone: true,
+//   imports: [FormsModule],
+//   templateUrl: './loan-application.component.html',
+//   styleUrl: './loan-application.component.scss',
+// })
+// export class LoanApplicationComponent {
+//   application: Application = new Application();
+//   loan: Loan = new Loan();
+
+//   masterService = inject(MainService);
+
+//   addLoan() {
+//     const strObj = JSON.stringify(this.loan);
+//     const newObj = JSON.parse(strObj);
+//     this.application.Loans.unshift(newObj);
+//   }
+
+//   onSubmit() {
+//     this.masterService.addNewApplication(this.application).subscribe(
+//       (Result: ApiResponseModel) => {
+//         if (Result.result) {
+//           alert('Loan Application Success');
+//         } else {
+//           alert(Result.message);
+//         }
+//       },
+//       (error) => {
+//         alert(error);
+//       }
+//     );
+//   }
+// }
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {
@@ -6,31 +48,37 @@ import {
   Loan,
 } from '../../model/application.model';
 import { MainService } from '../../service/main.service';
+
 @Component({
   selector: 'app-loan-application',
   standalone: true,
   imports: [FormsModule],
   templateUrl: './loan-application.component.html',
-  styleUrl: './loan-application.component.scss',
+  styleUrls: ['./loan-application.component.scss'],
 })
 export class LoanApplicationComponent {
   application: Application = new Application();
   loan: Loan = new Loan();
-
   masterService = inject(MainService);
 
   addLoan() {
     const strObj = JSON.stringify(this.loan);
     const newObj = JSON.parse(strObj);
     this.application.Loans.unshift(newObj);
+    // Reset the loan object to clear the form after adding
+    this.loan = new Loan();
   }
 
   onSubmit() {
+    // Add the loan to the application before submitting
+    this.addLoan();
+
     this.masterService.addNewApplication(this.application).subscribe(
       (Result: ApiResponseModel) => {
         if (Result.result) {
           alert('Loan Application Success');
-          this.updateLoanTable();
+          // Optionally, reset the application object here if needed
+          this.application = new Application();
         } else {
           alert(Result.message);
         }
@@ -39,8 +87,5 @@ export class LoanApplicationComponent {
         alert(error);
       }
     );
-  }
-  updateLoanTable() {
-    this.application.Loans.unshift(this.loan);
   }
 }
