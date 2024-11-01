0d8bfc2538f9781ece631397d4ad3e343cb3abd6
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-11-01 15:39:21 -0400

    nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './pages/nav-bar/nav-bar.component';
 
-
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NavBarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'LoanApplicationForm';
--- src/app/pages/nav-bar/nav-bar.component.ts
+++ src/app/pages/nav-bar/nav-bar.component.ts
@@ -1,13 +1,50 @@
-import { Component } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+// import { Component } from '@angular/core';
+// import { Router, RouterModule } from '@angular/router';
+
+// @Component({
+//   selector: 'app-nav-bar',
+//   standalone: true,
+//   imports: [RouterModule],
+//   templateUrl: './nav-bar.component.html',
+//   styleUrl: './nav-bar.component.scss',
+// })
+// export class NavBarComponent {
+
+// }
+import { Component, HostListener } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.scss'
+  styleUrls: ['./nav-bar.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)' }),
+        animate('200ms ease-in', style({ transform: 'translateX(0%)' })),
+      ]),
+      transition(':leave', [
+        animate('200ms ease-in', style({ transform: 'translateX(-100%)' })),
+      ]),
+    ]),
+  ],
 })
 export class NavBarComponent {
+  isMenuOpen = false;
 
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    if (window.innerWidth > 768 && this.isMenuOpen) {
+      this.isMenuOpen = false;
+    }
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
